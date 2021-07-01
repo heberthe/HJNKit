e1ae4358d83344a76b0adf93b56ac0bd2fb19d9c
Hebert.He <lengfenghhx@hotmail.com>
2021-07-01 13:54:49 +0800

    Initial Commit

---
+++ HJNKit/HJNKit.h
@@ -0,0 +1,18 @@
+//
+//  HJNKit.h
+//  HJNKit
+//
+//  Created by Hebert on 2021/7/1.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HJNKit.
+FOUNDATION_EXPORT double HJNKitVersionNumber;
+
+//! Project version string for HJNKit.
+FOUNDATION_EXPORT const unsigned char HJNKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HJNKit/PublicHeader.h>
+
+
